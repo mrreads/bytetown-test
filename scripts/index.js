d0c42a8ce99a051487f71780b2ca7e4bfab95d82
Alexander <mrreads@yandex.com>
2023-01-15 16:05:36 +0300

    header

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,12 @@ import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+
+let menuIsOpen = false;
+const header = document.querySelector('.header');
+const burger = document.querySelector('.header__icon.burger');
+burger.addEventListener('click', () => {
+    menuIsOpen = !menuIsOpen;
+    header.dataset.opened = menuIsOpen;
+});
