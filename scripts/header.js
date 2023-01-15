be0c00208cada7923eedbac1b1ea5f7a60b3a051
Alexander <mrreads@yandex.com>
2023-01-15 19:50:24 +0300

    gallery scroll

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,18 @@
 let menuIsOpen = false;
 const header = document.querySelector('.header');
 const burger = document.querySelector('.header__icon.burger');
+
 burger.addEventListener('click', () => {
     menuIsOpen = !menuIsOpen;
     header.dataset.opened = menuIsOpen;
+});
+
+document.body.addEventListener('click', (e) => {
+    let clickOnNavigation = e.target.closest('.header nav');
+    let clickOnBurger = e.target.closest('.header__icon.burger')
+    if (!clickOnBurger && !clickOnNavigation)
+    {
+        menuIsOpen = false;
+        header.dataset.opened = menuIsOpen;
+    }
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,18 @@
 import './header';
 import './animations';
+
+import ScrollBooster from 'scrollbooster';
+
+const menuWrapper = new ScrollBooster({
+    viewport: document.querySelector('.menu .menu-items'),
+    content: document.querySelector('.menu .menu-items'),
+    scrollMode: 'native',
+    direction: 'horizontal',
+    emulateScroll: true,
+});
+
+const menuScroll = document.querySelector('.menu .menu-next');
+menuScroll.addEventListener('click', () => {
+    let offset = menuWrapper.getState().position.x;
+    menuWrapper.scrollTo({ x: offset + 400 });
+});
